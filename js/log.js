7206966141f5edf4c64ace14f604aab7cc717941
anupammaurya6767 <anupammaurya6767@gmail.com>
2022-04-16 15:00:46 +0530

    added

--- js/log.js
+++ js/log.js
@@ -1,5 +1,19 @@
 document.getElementById("btn-log").addEventListener('click',() => {
     // To remove all sessions 
-    sessionStorage.clear();  
-    window.location.href = "login.html";
+    WebSecurity.Logout();
+    if (getCookie("UsingGoogleSignIn") === "1")
+    {
+        gapi.load('auth2', function() {
+            gapi.auth2.init();
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function(){
+                alert("Successfully Logged Out");
+                window.location = "login.html";
+            });
+        });
+    }
+    sessionStorage.clear();
+   
+    
+    window.localStorage.clear();
 })
