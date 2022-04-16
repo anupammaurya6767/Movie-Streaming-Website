b564ec58a439ec0d8ef41fc1301c011cbb60be3b
Anupam Maurya <anupammaurya6767@gmail.com>
2022-04-16 15:14:09 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,5 +1,22 @@
 document.getElementById("btn-log").addEventListener('click',() => {
     // To remove all sessions 
-    sessionStorage.clear();  
-    window.location.href = "login.html";
+    document.getElementById("btn-log").addEventListener('click',() => {
+    // To remove all sessions 
+    WebSecurity.Logout();
+    if (getCookie("UsingGoogleSignIn") === "1")
+    {
+        gapi.load('auth2', function() {
+            gapi.auth2.init();
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function(){
+                alert("Successfully Logged Out");
+                window.location = "login.html";
+            });
+        });
+    }
+    sessionStorage.clear();
+   
+    
+    window.localStorage.clear();
+})
 })
