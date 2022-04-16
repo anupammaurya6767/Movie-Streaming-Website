46975d61aab7ae0da36018195ec9b9d2229a702a
Anupam Maurya <anupammaurya6767@gmail.com>
2022-04-16 15:44:31 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -12,7 +12,7 @@
 })
 //     gapi.auth2.getAuthInstance().signIn({
 //     prompt: 'select_account'
-});
+// });
 //     var auth2 = gapi.auth2.getAuthInstance();
 // auth2.signOut().then(function () {
 // });
