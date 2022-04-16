bcdf1580d0d534ffca3f15fa5ac3411af1622686
Anupam Maurya <anupammaurya6767@gmail.com>
2022-04-16 15:37:56 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -5,32 +5,32 @@
 auth2.signOut().then(function () {
 });
 auth2.disconnect();
-    gapi.auth.signOut()
-    localStorage.clear();
-    console.log("hi");
-    google.accounts.id.disableAutoSelect();
-    googleSignInClient.signOut();
-                gapi.load('auth2', function() {
-            gapi.auth2.init();
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function(){
-                console.log("Successfully Logged Out");
-                window.location = "login.html";
-            });
-        });
-    if (getCookie("UsingGoogleSignIn") === "1")
-    {
-        gapi.load('auth2', function() {
-            gapi.auth2.init();
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function(){
-                alert("Successfully Logged Out");
-                window.location = "login.html";
-            });
-        });
-    }
+//     gapi.auth.signOut()
+//     localStorage.clear();
+//     console.log("hi");
+//     google.accounts.id.disableAutoSelect();
+//     googleSignInClient.signOut();
+//                 gapi.load('auth2', function() {
+//             gapi.auth2.init();
+//             var auth2 = gapi.auth2.getAuthInstance();
+//             auth2.signOut().then(function(){
+//                 console.log("Successfully Logged Out");
+//                 window.location = "login.html";
+//             });
+//         });
+//     if (getCookie("UsingGoogleSignIn") === "1")
+//     {
+//         gapi.load('auth2', function() {
+//             gapi.auth2.init();
+//             var auth2 = gapi.auth2.getAuthInstance();
+//             auth2.signOut().then(function(){
+//                 alert("Successfully Logged Out");
+//                 window.location = "login.html";
+//             });
+//         });
+//     }
     sessionStorage.clear();
-   
+    window.location = "login.html";
     
     window.localStorage.clear();
 })
