18073278a00e3e0b8d9d771422131681556a1db8
Anupam Maurya <anupammaurya6767@gmail.com>
2022-04-16 15:42:05 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,8 +1,17 @@
 
   document.getElementById("btn-log").addEventListener('click',() => {
     // To remove all sessions
-    gapi.auth2.getAuthInstance().signIn({
-    prompt: 'select_account'
+    Auth.signOut()
+  .then(res => {
+    console.log('res ', res) // would be undefined
+    /** Other logic **/
+})
+  .catch((e) => {
+      console.log(e);
+    /** Catch logic **/
+})
+//     gapi.auth2.getAuthInstance().signIn({
+//     prompt: 'select_account'
 });
 //     var auth2 = gapi.auth2.getAuthInstance();
 // auth2.signOut().then(function () {
