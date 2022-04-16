8c73d05ffe10284ef698a41f17bf1f3fbde4d378
Anupam Maurya <anupammaurya6767@gmail.com>
2022-04-16 15:16:01 +0530

    Update log.js

--- js/log.js
+++ js/log.js
@@ -3,6 +3,14 @@ document.getElementById("btn-log").addEventListener('click',() => {
     document.getElementById("btn-log").addEventListener('click',() => {
     // To remove all sessions 
     WebSecurity.Logout();
+                gapi.load('auth2', function() {
+            gapi.auth2.init();
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function(){
+                console.log("Successfully Logged Out");
+                window.location = "login.html";
+            });
+        });
     if (getCookie("UsingGoogleSignIn") === "1")
     {
         gapi.load('auth2', function() {
